0d09624ff58810748c60ed4393494220b6910f61
Marios Tsekitsidis <tseki@iastate.edu>
2022-11-15 20:23:27 -0600

    Add button with fetch and console output

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({data: []});
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+
+  const handleClick = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('https://www.zotero.org/oauth/authorize', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="App">
-      hello
+      <button onClick={handleClick}>Connect</button>
+
+      {isLoading && <h2>Loading...</h2>}
     </div>
   );
 }
